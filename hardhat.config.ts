8ebc5fc9fac6b6fc50f273bcc9bd4b233539221a
Ajidokwu Sabo <realjaiboi70@gmail.com>
2025-09-02 14:37:10 +0100

    cohort 7  staking contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,15 +7,15 @@ module.exports = {
     hardhat: {},
 
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/KkPRZRdLfkaSApEMmxlTiWgKIBkRymzD ",
-      accounts: ['6801c8019e9e9b1266468b47d94baed8b30593a03b1a2e3d8f4cba35b0a11650'],
+      url: "rpc url",
+      accounts: ['private key'],
       
     },
   },
   lockGasLimit: 200000000000,
   gasPrice: 10000000000,
   etherscan: {
-    apiKey: "9D2E6X9UM39NEUI1T9UPCGZR9CTGNJF5SN",
+    apiKey: "api key",
   },
 
   sourcify: {
